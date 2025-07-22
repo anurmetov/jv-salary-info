7bf1f671deb12da61f3a0f9863ed2bcbf6ddb3a9
artem <artemnurmetow@gmail.com>
2025-07-22 17:06:19 +0200

    feat(core): implement SalaryInfo class to generate salary reports
    - Added SalaryInfo class with `getSalaryInfo` method to calculate total salary
    - for each employee within a specified date range. The report is formatted with employee names and their respective total salaries for the given period.

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,37 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        LocalDate from = LocalDate.parse(dateFrom, DATE_TIME_FORMATTER);
+        LocalDate to = LocalDate.parse(dateTo, DATE_TIME_FORMATTER);
+
+        int[] salaryPerEmployee = new int[names.length];
+        // Get each employee and check if its date is valid
+        for (String date : data) {
+            String[] parts = date.split(" ");
+            LocalDate employeeDate = LocalDate.parse(parts[0], DATE_TIME_FORMATTER);
+            if (employeeDate.isAfter(from) && employeeDate.isBefore(to)) {
+                // Iterating through names to get each salary
+                for (int j = 0; j < names.length; j++) {
+                    if (parts[1].equals(names[j])) {
+                        salaryPerEmployee[j] += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
+                    }
+                }
+            }
+        }
+
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo).append(System.lineSeparator());
+        for (int i = 0; i < salaryPerEmployee.length; i++) {
+            result.append(names[i]).append(" - ").append(salaryPerEmployee[i]).append(System.lineSeparator());
+        }
+
+        return result.toString();
     }
 }
